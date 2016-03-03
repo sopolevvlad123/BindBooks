895465f1dfc7309ba964ba13a89012fe05a88bda
Sopolev Vlad <sopolevvlad1989@gmail.com>
2016-03-03 11:45:19 +0200

    Controller fixing

---
+++ src/main/java/com/service/BindService.java
@@ -0,0 +1,7 @@
+package com.service;
+
+/**
+ * Created by pc9 on 03.03.16.
+ */
+public class BindService {
+}
--- src/main/java/com/service/JsonService.java
+++ src/main/java/com/service/JsonService.java
@@ -4,24 +4,54 @@ import com.entity.Book;
 import com.entity.BookIrbisHtml;
 import org.json.JSONString;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
  * Created by pc9 on 27.11.15.
  */
 @Service
-public class JsonWrappingServise {
-    public JsonWrappingServise() {
+public class JsonService {
+
+    @Autowired
+    private DownloadBookService downloadBookService;
+
+
+    public JsonService() {
     }
+
+
+    public String getBook(Integer bookIndex, HttpSession session, Model model){
+
+            List<Book>  bookList = (List<Book>) session.getAttribute("bookLi");
+            Book book = null;
+
+            if (bookIndex != null) {
+                book = bookList.get(bookIndex);
+            } else {
+                book = bookList.get(0);
+            }
+            model.addAttribute("bookIndex", bookIndex);
+
+            downloadBookService.download(String.valueOf(book.getBookId()));
+
+            return getJsonString(book);
+    }
+
+
+
+
 
     /**
      * method converts Book obj to Json string
      * @param book
      * @return
      */
-    public String getJsonString(Book book) {
+      private String getJsonString(Book book) {
         JSONObject jsObject = new JSONObject();
 
         jsObject.put("name", book.getEname());
